daf21dcb722b9ea699634bbb23cc648a6b8ef09c
Dalei Sun <daleisun@Daleis-MacBook-Pro.local>
2023-12-07 23:33:37 +0800

    feature: add producer

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -1,8 +1,38 @@
 package io.conduktor.demos.kafka;
 
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProducerDemo {
-    
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        LOG.info("Hello World!");
+
+        // Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:29092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // Create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create a Producer Record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("demo_java", "hello world");
+
+        // Send data
+        producer.send(producerRecord);
+
+        // Flush and close the producer
+        producer.flush();
+        producer.close();
     }
 }
