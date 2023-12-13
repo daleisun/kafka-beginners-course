aa184dd03453f09cf4edd466832d11f5cb560e2d
Dalei Sun <sundalei2011@163.com>
2023-12-13 23:59:54 +0800

    feature: implement producers and consumers

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java
@@ -0,0 +1,50 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        LOG.info("I am a Kafka Consumer!");
+
+        String groupId = "my-java-application";
+        String topic = "demo_java";
+
+        // Create Consumer Properties
+        Properties properties = new Properties();
+
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // Create a consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // Subscribe to a topic
+        consumer.subscribe(List.of(topic));
+
+        // Poll for data
+        while (true) {
+            LOG.info("Polling");
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+
+            records.forEach(record -> {
+                LOG.info("Key: " + record.key() + ", Value: " + record.value());
+                LOG.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            });
+        }
+    }
+}
---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoKeys.java
@@ -0,0 +1,69 @@
+package io.conduktor.demos.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemoKeys {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
+
+    public static void main(String[] args) {
+
+        LOG.info("I am a producer!");
+
+        // Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // Create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int j = 0; j < 2; j++) {
+
+            for (int i = 0; i < 10; i++) {
+
+                String topic = "demo_java";
+                String key = "id_" + i;
+                String value = "hello world " + i;
+
+                // Create a Producer Record
+                ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(topic, key, value);
+
+                // Send data
+                producer.send(producerRecord, new Callback() {
+
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception exception) {
+
+                        if (exception == null) {
+                            LOG.info("Key: " + key + " | Partition: " + metadata.partition());
+                        } else {
+                            LOG.error("Error while producing", exception);
+                        }
+                    }
+
+                });
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        // Flush and close the producer
+        producer.flush();
+        producer.close();
+    }
+}
