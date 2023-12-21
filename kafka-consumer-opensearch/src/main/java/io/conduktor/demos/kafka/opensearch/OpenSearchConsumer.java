b2b30c7de54572a55ff67d25975f8c33616d46e1
Dalei Sun <sundalei2011@163.com>
2023-12-21 10:59:23 +0800

    feature: add module for opensearch

---
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -0,0 +1,9 @@
+package io.conduktor.demos.kafka.opensearch;
+
+public class OpenSearchConsumer {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("hello world");
+    }
+}
---
+++ kafka-consumer-opensearch/src/test/java/io/conduktor/demos/AppTest.java
@@ -0,0 +1,13 @@
+package io.conduktor.demos;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
